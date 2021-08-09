11ea3fdff3a2579d690d514b51c1e4245197bc42
isaacjlivingston <52447975+isaacjlivingston@users.noreply.github.com>
2021-08-09 15:42:10 +0200

    Adding git to dockerfile and setting GITHUB_WORKSPACE as workspace

--- cli/scripts/bin/entrypoint.sh
+++ cli/scripts/bin/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-baseURL="$baseURL/$ACCOUNT_ID"
-authToken="$AUTH_TOKEN"
-WORKSPACE="$pwd"
+export baseURL="$baseURL/$ACCOUNT_ID"
+export WORKSPACE="$GITHUB_WORKSPACE"
 env
 
 source bin/dynamicScriptGitJobBuilder.sh commit_id="$GITHUB_SHA" env="$ENV" conf_folder="$CONF_FOLDER"
