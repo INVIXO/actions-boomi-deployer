6f70bb0f33517205c99ff09457ba5acb7e46995f
isaacjlivingston <52447975+isaacjlivingston@users.noreply.github.com>
2021-08-10 16:17:39 +0200

    Removing env call from entrypoint

--- cli/scripts/bin/entrypoint.sh
+++ cli/scripts/bin/entrypoint.sh
@@ -2,6 +2,5 @@
 
 export baseURL="$baseURL/$ACCOUNT_ID"
 export WORKSPACE="$GITHUB_WORKSPACE"
-env
 
 source bin/dynamicScriptGitJobBuilder.sh commit_id="$GITHUB_SHA" env="$ENV" conf_folder="$CONF_FOLDER"
