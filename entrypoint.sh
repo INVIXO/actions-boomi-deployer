417a4b559675cb754ecf01322c8063e0e5e23737
isaacjlivingston <52447975+isaacjlivingston@users.noreply.github.com>
2021-08-11 16:18:52 +0200

    Erroring if source script fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 export baseURL="$baseURL/$ACCOUNT_ID"
 export WORKSPACE="$GITHUB_WORKSPACE"
 
-cd .
-ls
-cd "${GITHUB_WORKSPACE}"
-ls
-cd "${SCRIPTS}"
-
 source bin/dynamicScriptGitJobBuilder.sh commit_id="$GITHUB_SHA" env="$ENV" conf_folder="$CONF_FOLDER"
+
+result=$?
+
+if [ $result -ne 0 ]; then
+    exit $result
+fi
