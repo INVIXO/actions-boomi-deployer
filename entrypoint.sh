21005201ec2af807b928b41bd071cd5e18d8ae52
isaacjlivingston <52447975+isaacjlivingston@users.noreply.github.com>
2021-08-11 16:21:58 +0200

    Adding cd back in

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 export baseURL="$baseURL/$ACCOUNT_ID"
 export WORKSPACE="$GITHUB_WORKSPACE"
 
+cd "${SCRIPTS}"
+
 source bin/dynamicScriptGitJobBuilder.sh commit_id="$GITHUB_SHA" env="$ENV" conf_folder="$CONF_FOLDER"
 
 result=$?
